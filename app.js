d52936f99cbb2fabdc85a6919f265500df9b2cb0
RianSilvaCCOK <rian.silva@sptech.school>
2026-01-22 12:48:52 -0300

    ultima atualização

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var indexRouter = require("./src/routes/index");
 var livrosRouter = require("./src/routes/livros");
 var autoresRouter = require("./src/routes/autores");
 var generosRouter = require("./src/routes/generos");
+var autorRouter = require("./src/routes/quantidade")
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -25,6 +26,7 @@ app.use("/", indexRouter);
 app.use("/livros", livrosRouter);
 app.use("/autores", autoresRouter);
 app.use("/generos", generosRouter);
+app.use("/quantidade", autorRouter)
 
 app.listen(PORTA_APP, function () {
     console.log(`
